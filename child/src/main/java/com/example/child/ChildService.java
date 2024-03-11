346ad75cf5ece27bc50b51fd3bb5b329e7e07cf8
Mallika-Dey <mallikaju307@gmail.com>
2024-03-11 19:34:47 +0600

    added opentelemetry

--- child/src/main/java/com/example/child/ChildService.java
+++ child/src/main/java/com/example/child/ChildService.java
@@ -1,13 +1,21 @@
 package com.example.child;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
  * @author Mallika Dey
  */
 @Service
+@Slf4j
 public class ChildService {
     public String childService() {
+        log.info("childservice executing................");
+        return testChildService();
+    }
+
+    public String testChildService() {
+        log.info("test child service executing................");
         return "child service.......";
     }
 }
